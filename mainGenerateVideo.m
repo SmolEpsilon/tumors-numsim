5c868e85869f5dfdcf8677aba3c94ac8c890ad61
Kristos Qiqi <46170170+SmolEpsilon@users.noreply.github.com>
2023-03-07 15:12:25 +0100

    Update mainGenerateVideo.m

--- mainGenerateVideo.m
+++ mainGenerateVideo.m
@@ -1,3 +1,4 @@
+%%
 % mainGenerateVideo
 % This script loads a segmented 3D-image of a brain, then places a
 % synthetic tumour and lets it grow according to the PDE-model
@@ -6,41 +7,66 @@
 %
 % This script solves the mathematical model with p=2, rho=const, a=b=c=1,
 % D(x)=d(x)*eye. Synthetic tumour is planted in.
-
-% Reset matlab variables and close all figures
+%%
 close all
 clear
+%Gunzip files
+files = gunzip('MTP_2023_0004_3T1_seg.nii.gz');
+%files_seg = gunzip('BraTS_version\BraTS2021_01516_seg.nii.gz','BraTS_version\');
+segvol1 = niftiread("MTP_2023_0004_3T1_seg.nii");
+%vol1 = niftiread('Unziped_Dataset\MTP_2023_0004\T1.nii');
+segvol2 = niftiread('T2_half_seg.nii');
+segtumor_flip = flip(niftiread('BraTS2021_01516_seg.nii'));
+seg_tumor = niftiread('BraTS2021_01516_seg.nii');
+sug_tumor_twice = seg_tumor + segtumor_flip;
+%%
+%Find coordinates for segmented tumor
+nonzero_indices = find(seg_tumor); % find indices of non-zero elements
+
+% convert linear indices to subscripts
+[Sub1, Sub2, Sub3] = ind2sub(size(segtumor_flip),nonzero_indices);
+sub1size = size(Sub1);
+sub2size = size(Sub2);
+sub3size = size(Sub3);
+
+xmid = Sub1(round(sub1size(1)/2));
+ymid = Sub2(round(sub2size(1)/2));
+zmid = Sub3(round(sub3size(1)/2));
+
+
+%%
+
 
 % Parameters used in the model
 % High-Grade Gliomas (HGG)
-rho = 0.012; % Proliferation rate
+rho = 0.3; % Proliferation rate
 dw  = 0.65;  % Diffusion coefficient, white matter
 dg  = 0.13;  % Diffusion coefficient, grey matter
-
+beta = 1;
+alpha = 1;
+gamma = 1;
 % PDE-solver parameters
-tStep  = 0.05;    % Discretization step for variable t
-noIter = 1000;    % Number of iterations in t-variable
-freqImgSave = 10; % Every 10th image is to be saved (adjust FrameRate for myVideo1 and myVideo2 below accordingly)
+tStep  = 0.05;    % Discretization step for variable t (days)
+noIter = 300;    % Number of iterations in t-variable
+freqImgSave = 1; % Every 10th image is to be saved (adjust FrameRate for myVideo1 and myVideo2 below accordingly)
 
 % Load brain-data
-load('coreg_7940_mni152_2009bet.mat', 'segvol', 'grayMatterSegmentationValues', 'whiteMatterSegmentationValues');
+%load('coreg_7940_mni152_2009bet.mat', 'segvol', 'grayMatterSegmentationValues', 'whiteMatterSegmentationValues');
 % segvol contains a segmented 3D-scan of a brain (256x256x256 voxels)
 % ...MatterSegmentationValues contain a list of values in segvol that
 %                    correspond to grey and whitematter, respectively
 
 % Generate the D-function that appears in the PDE
-D =   dg * ismember(segvol, grayMatterSegmentationValues) ...
-    + dw * ismember(segvol, whiteMatterSegmentationValues);
-
-% Crop D so that it is not surrounded by unnecessary/dummy zero voxels
-idx1 = squeeze(any(any(D, 2), 3));
-idx2 = squeeze(any(any(D, 1), 3));
-idx3 = squeeze(any(any(D, 1), 2));
-D = D(idx1, idx2, idx3);
-
-% D however needs to be surrounded by at least one dummy layer of zeroes
-% due to boundary conditions of the model
-D = padarray(D, [1, 1, 1], 0, 'both');
+D =   dg * ismember(segvol1,2) + dw * ismember(segvol1,3);
+% % Crop D so that it is not surrounded by unnecessary/dummy zero voxels
+% idx1 = squeeze(any(any(D, 2), 3));
+% idx2 = squeeze(any(any(D, 1), 3));
+% idx3 = squeeze(any(any(D, 1), 2));
+% D = D(idx1, idx2, idx3);
+% 
+% % D however needs to be surrounded by at least one dummy layer of zeroes
+% % due to boundary conditions of the model
+% D = padarray(D, [1, 1, 1], 0, 'both');
 
 % Generate Omega, the domain in which the PDE is being solved
 Om = +(D > 0);
@@ -49,12 +75,12 @@ OmSize = size(Om);
 % Plant a seed of a tumour
 if false  % Change to true if the seed is to be placed randomly. If false place the tumour "in the middle"
     idxList = find(Om);
-    seedDiam   = 1 + 2*randi([5, 8]);
+    seedDiam   = 19;
     [seedPos(1), seedPos(2), seedPos(3)] = ind2sub(OmSize, idxList(randi([1, length(idxList)])));
 else    
     % Position and diameter of the seed
-    seedPos    = round(OmSize/2);
-    seedDiam   = 13;
+    seedPos    = [157,141,102];
+    seedDiam   =10;
     % make sure that the seed is planted in Om
     seedTmp = find(D(seedPos(1), seedPos(2), :));
     seedTmp(seedTmp < seedPos(3)) = [];
@@ -70,34 +96,34 @@ u(u<1e-3) = 0;
 u = u .* Om;
 phi = u;
 
-% Show a 3D-image of the brain and the planted seed
-fig1 = figure(1);
-fig1.Position = [10, 200, 1280, 720];
-
-myVideo1 = VideoWriter('tumGrowth.mp4', 'MPEG-4');
-myVideo1.FrameRate = 10;
-myVideo1.Quality = 98;
-open(myVideo1);
-
-tumorPatch = visTum3d(D, u, seedPos);
-drawnow;
-
-frame1 = getframe(fig1);
-writeVideo(myVideo1, frame1);
-
-% Show a 3D-image of the tumour only
-fig2 = figure(2);
-fig2.Position = [10, 200, 1280, 720];
-
-myVideo2 = VideoWriter('tumOnlyGrowth.mp4', 'MPEG-4');
-myVideo2.FrameRate = 10;
-myVideo2.Quality = 98;
-open(myVideo2);
-
-tumorOnlyPatch = visTumOnly3d(u);
-drawnow;
-frame2 = getframe(fig2);
-writeVideo(myVideo2, frame2);
+% %Show a 3D-image of the brain and the planted seed
+% fig1 = figure(1);
+% fig1.Position = [-1599, -199, 500, 500];
+% 
+% myVideo1 = VideoWriter('tumGrowth.mp4', 'MPEG-4');
+% myVideo1.FrameRate = 10;
+% myVideo1.Quality = 98;
+% open(myVideo1);
+% 
+% tumorPatch = visTum3d(D, u, seedPos);
+% drawnow;
+% 
+% frame1 = getframe(fig1);
+% writeVideo(myVideo1, frame1);
+% 
+% %Show a 3D-image of the tumour only
+% fig2 = figure(2);
+% fig2.Position = [-1000, -100, 500, 500];
+% 
+% myVideo2 = VideoWriter('tumOnlyGrowth.mp4', 'MPEG-4');
+% myVideo2.FrameRate = 10;
+% myVideo2.Quality = 98;
+% open(myVideo2);
+% 
+% tumorOnlyPatch = visTumOnly3d(u);
+% drawnow;
+% frame2 = getframe(fig2);
+% writeVideo(myVideo2, frame2);
 
 % Prepare auxilliary variables for Finite Difference Method
 % Edges of Omega (needed for the Neumann condition)
@@ -119,29 +145,90 @@ Davg.Neg2 = (circshift(D,  1, 2) + D)/2;
 Davg.Pos3 = (circshift(D, -1, 3) + D)/2;
 Davg.Neg3 = (circshift(D,  1, 3) + D)/2;
 
+avg_density = zeros(noIter,1);
+
+fig3 = figure(3);
+cmin = 0.0; % Minimum value
+cmax = 1.0; % Maximum value
 % run the model, i.e., solve the PDE
 for currIter=1:noIter
-  fprintf('Iteration %d\n', currIter);
-  u = u + tStep * (divDdu(u, Davg, OmEdges) + rho * u .* (1-u));
-  if mod(currIter, freqImgSave) == 0
-    figure(fig1);
-    tumorPatch = visTum3dUpdate(u, tumorPatch);
-    drawnow;
-    frame1 = getframe(fig1);
-    writeVideo(myVideo1, frame1);
-
-    figure(fig2);
-    tumorOnlyPatch = visTum3dUpdate(u, tumorOnlyPatch);
-    drawnow;
-    frame2 = getframe(fig2);
-    writeVideo(myVideo2, frame2);
-  end
+fprintf('Iteration %d\n', currIter);
+u = u + tStep * (divDdu(u, Davg, OmEdges,2) + rho * (u.^alpha)*beta .* ...
+(1-u.^(1/beta)).^gamma);
+fprintf('Sum u: %f\n', sum(u(:) .* (u(:)>0.16) )./(sum(u(:)>0.16)));
+avg_density(currIter) = sum(u(:) .* (u(:)>0.16) )./(sum(u(:)>0.16));
+%  fprintf('Sum u08: %f\n', sum(u(:)>0.08));
+%  fprintf('Sum u16: %f\n', sum(u(:)>0.16));
+%  fprintf('Sum u24: %f\n', sum(u(:)>0.24));
+figure(fig3);
+imagesc(u(:,:,102),[cmin, cmax]);
+clim([cmin, cmax])
+colormap jet; 
+colorbar; 
+axis equal; 
+title(sprintf('Iteration %d\n',currIter)); % Add a title with the current time step
+drawnow; 
+%    if mod(currIter, freqImgSave) == 0
+%     figure(fig1);
+%       tumorPatch = visTum3dUpdate(u, tumorPatch);
+%       drawnow;
+%       frame1 = getframe(fig1);
+%       writeVideo(myVideo1, frame1);
+% 
+%      figure(fig2);
+%       tumorOnlyPatch = visTum3dUpdate(u, tumorOnlyPatch);
+%       drawnow;
+%       frame2 = getframe(fig2);
+%       writeVideo(myVideo2, frame2);
+%   end
 end
 
-writeVideo(myVideo1, frame1);
-close(myVideo1);
+% writeVideo(myVideo1, frame1);
+% close(myVideo1);
+% 
+% writeVideo(myVideo2, frame2);
+% close(myVideo2);
+% 
+% save('reactDiffModelSolution.mat', 'phi', 'u', 'seedPos', 'seedDiam', ...
+%     'D', 'Om', 'tStep', 'noIter');
 
-writeVideo(myVideo2, frame2);
-close(myVideo2);
+%%
+%Add tumor in Omega
+u_th = +(u>0.02);
+Om_wt = ceil(Om + u_th);
 
-save('reactDiffModelSolution.mat', 'phi', 'u', 'seedPos', 'seedDiam', 'D', 'Om', 'tStep', 'noIter');
+
+%%
+% Find the volume of synthetic tumor
+% 1 voxel = 1mm^3
+nonzero_indices = find(u>0.16); % find indices of non-zero elements
+[num_nonzero, ~] = size(nonzero_indices); % count non-zero elements
+
+% convert linear indices to subscripts
+[sub1, sub2, sub3] = ind2sub(size(u),nonzero_indices);
+
+% print the subscripts of non-zero elements
+disp("Subscripts of non-zero elements in synthetic tumor:");
+disp([sub1, sub2, sub3]);
+
+
+%%
+%Find the volume of tumor
+% 1 voxel = 1mm^3
+nonzero_indices2 = find(seg_tumor); % find indices of non-zero elements
+[num_nonzero2, ~] = size(nonzero_indices2); % count non-zero elements
+
+% convert linear indices to subscripts
+[sub12, sub22, sub32] = ind2sub(size(seg_tumor),nonzero_indices2);
+
+% print the subscripts of non-zero elements
+disp("Subscripts of non-zero elements in synthetic tumor:");
+disp([sub12, sub22, sub32]);
+
+%print the number of non-zero elements
+disp("Number of non-zero elements in synthetic tumor:");
+disp(num_nonzero)
+disp("Sum of non-zero elements in synthetic tumor:");
+disp(sum(u(:)))
+disp("Number of non-zero elements in real tumor")
+disp(num_nonzero2);
